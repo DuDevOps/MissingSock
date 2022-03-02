74820635dddc6beefb5a8fca963fa7249a8bbdbc
Billy <koosbilly9@gmail.com>
2022-03-02 15:33:41 +0200

    add max-height 200px to reports

--- static/rep_animal_register.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  console.log("rep_animal_register.js loaded")
-});
-
-function search_for_asset_no(search_box_id) {
-
-  Window.find(document.getElementById(search_box_id).value);
-
-}
